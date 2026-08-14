19bb94ab01427a1ffcf2825f7fadf856da439f83
richarddavidowen <richarddavidowen@gmail.com>
2026-08-14 22:43:28 +0100

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import './globals.css';
+export const metadata={title:'Star Trek Books',description:'Star Trek book catalogue and reading tracker'};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
